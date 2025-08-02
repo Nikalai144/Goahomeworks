f0b7f979ac4d43ad5dfdce28c13e168622f87b6d
Nikalai144 <nikolozagvarjaladze@gmail.com>
2025-08-02 13:33:53 +0400

    second commit

---
+++ level 48/classwork/u.py
@@ -0,0 +1,3 @@
+#1) https://www.codewars.com/kata/50654ddff44f800200000004
+def multiplyer(a,b):
+    return a * b
---
+++ level 48/homework/in.py
@@ -0,0 +1,24 @@
+#1) https://www.codewars.com/kata/57356c55867b9b7a60000bd7/train/python
+def basic_op(operator, value1, value2):
+    if operator == "+":
+        return value1 + value2
+    if operator == "-":
+        return value1 - value2
+    if operator == "*":
+        return value1 * value2
+    if operator == "/":
+        return value1 / value2
+#2) https://www.codewars.com/kata/55cbc3586671f6aa070000fb/train/python
+def check_for_factor(base, factor):
+    return base % factor == 0
+#3)https://www.codewars.com/kata/57a2013acf1fa5bfc4000921/train/python
+#???
+#4)https://www.codewars.com/kata/50ee6b0bdeab583673000025/train/python
+a = "code"
+b = "wa.rs"
+name = a + b
+#5) https://www.codewars.com/kata/5ce9c1000bab0b001134f5af/train/python
+#???
+#6) https://www.codewars.com/kata/5625618b1fe21ab49f00001f/train/python
+def say_hello(name):
+    return "Hello, " + name
